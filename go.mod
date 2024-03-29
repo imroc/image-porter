80669cf2455aee7639c2bed5dbd407d6af2a4c63
roc <roc@imroc.cc>
2024-03-29 14:31:22 +0800

    sync tags if digest is updated

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- sync.go
+++ sync.go
@@ -7,6 +7,7 @@ import (
 	"os/exec"
 	"strings"
 
+	"github.com/Masterminds/semver/v3"
 	"k8s.io/klog/v2"
 )
 
@@ -32,13 +33,16 @@ func Sync(config *Config, retryAttempt int) error {
 		if err != nil {
 			klog.Infof("dst image have no tags: %s (%v)", image.To, err)
 		}
-		tags := tagsToSync(srcTags, dstTags)
-		if len(tags) == 0 {
-			klog.Infof("skip: all tags are already been synced for image %s", image.From)
-			continue
+		toSync, toUpdate := tagsToSync(srcTags, dstTags)
+		if len(toSync) > 0 {
+			klog.Infof("sync new tags for image %s: %v", image.From, toSync)
+			syncImageTags(image.From, image.To, toSync, retryAttempt)
+		} else {
+			klog.Infof("no new tags for image %s", image.From)
 		}
-		klog.Infof("sync tags for image %s: %v", image.From, tags)
-		syncImageTags(image.From, image.To, tags, retryAttempt)
+		if len(toUpdate) > 0 {
+			updateImageTags(image.From, image.To, toSync, retryAttempt)
+		}
 	}
 	return nil
 }
@@ -56,18 +60,23 @@ func listTags(image string) ([]string, error) {
 	return tags.Tags, nil
 }
 
-func tagsToSync(srcTags, dstTags []string) []string {
-	result := []string{}
+func tagsToSync(srcTags, dstTags []string) (toSync, toUpdate []string) {
 	if len(dstTags) == 0 {
-		return srcTags
+		toSync = srcTags
+		return
 	}
 	dstMap := sliceToMap(dstTags)
 	for _, tag := range srcTags {
-		if tag == "latest" || !dstMap[tag] {
-			result = append(result, tag)
+		if !dstMap[tag] {
+			toSync = append(toSync, tag)
+			continue
+		}
+		_, err := semver.NewVersion(tag)
+		if err != nil { // not sematic version, may update
+			toUpdate = append(toUpdate, tag)
 		}
 	}
-	return result
+	return
 }
 
 func sliceToMap[T comparable](slice []T) map[T]bool {
@@ -78,19 +87,51 @@ func sliceToMap[T comparable](slice []T) map[T]bool {
 	return m
 }
 
+func inspectDigest(image, tag string) (digest string, err error) {
+	args := []string{"inspect", "--format", "{{.Digest}}", fmt.Sprintf("docker://%s:%s", image, tag)}
+	cmd := exec.Command("skopeo", args...)
+	digestBytes, err := cmd.Output()
+	if err != nil {
+		klog.Errorf("failed to inspect %s:%s", image, tag)
+	}
+	digest = string(digestBytes)
+	return
+}
+
+func updateImageTags(imageFrom, imageTo string, tags []string, retryAttempt int) {
+	for _, tag := range tags {
+		digestFrom, err := inspectDigest(imageFrom, tag)
+		if err != nil {
+			continue
+		}
+		digestTo, err := inspectDigest(imageTo, tag)
+		if err != nil {
+			continue
+		}
+		if string(digestFrom) != string(digestTo) { // found tag update
+			klog.Infof("found tag update for image %s:%s", imageFrom, tag)
+			syncImageTag(imageFrom, imageTo, tag, retryAttempt)
+		}
+	}
+}
+
+func syncImageTag(imageFrom, imageTo, tag string, retryAttempt int) {
+	args := []string{"copy", "--all", fmt.Sprintf("docker://%s:%s", imageFrom, tag), fmt.Sprintf("docker://%s:%s", imageTo, tag)}
+	klog.Infof("execute command: skopeo %s", strings.Join(args, " "))
+	cmd := exec.Command("skopeo", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	var err error
+	for i := 0; i < retryAttempt; i++ {
+		err = cmd.Run()
+		if err == nil {
+			continue
+		}
+	}
+}
+
 func syncImageTags(imageFrom, imageTo string, tags []string, retryAttempt int) {
 	for _, tag := range tags {
-		args := []string{"copy", "--all", fmt.Sprintf("docker://%s:%s", imageFrom, tag), fmt.Sprintf("docker://%s:%s", imageTo, tag)}
-		klog.Infof("execute command: skopeo %s", strings.Join(args, " "))
-		cmd := exec.Command("skopeo", args...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		var err error
-		for i := 0; i < retryAttempt; i++ {
-			err = cmd.Run()
-			if err == nil {
-				continue
-			}
-		}
+		syncImageTag(imageFrom, imageTo, tag, retryAttempt)
 	}
 }
